b3bead76b832c7f99027772b5e2490ada77e5c98
Thanh <thanh.nguyen@mideasvn.com>
2020-04-20 21:23:16 +0700

    Update Field

--- projects/dynamic-field/src/lib/dynamic-field.service.ts
+++ projects/dynamic-field/src/lib/dynamic-field.service.ts
@@ -2,8 +2,6 @@ import { NgxFieldModel, FieldContext } from './field.model';
 import { Type } from '@angular/core';
 import { BaseFieldComponent } from './base-field.component';
 import { Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { FieldModel } from 'projects/dynamic-material-field/src/public-api';
 
 export interface FieldType<FielModel extends NgxFieldModel = NgxFieldModel> {
   typ: Type<BaseFieldComponent>,
